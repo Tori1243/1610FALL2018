2c7e65499c3be9f7dc18feaf9fb4a459a40bc15f
Unknown <Tori7809@gmail.com>
2018-09-19 15:28:17 -0600

    Health And Money
    Adding and Subtracting health and money

---
+++ cube project/Assets/MouseDownBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class MouseDownBehavior : MonoBehaviour
+{
+	public UnityEvent Event;
+	
+	private void OnMouseDown()
+	{
+		Event.Invoke();
+	}
+}
