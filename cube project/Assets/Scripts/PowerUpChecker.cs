a0562300319d5bc681addc2f4bda532182fe1752
Unknown <Tori7809@gmail.com>
2018-10-02 16:13:50 -0600

    AddLife If statement
    Had tutor help

---
+++ cube project/Assets/Scripts/PowerUpChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpChecker : MonoBehaviour
+{
+
+    public PowerUp_SO powerUp;
+    public string nametocheck;
+    public void Start()
+    {
+        CheckPowerUp();
+    }
+    public void CheckPowerUp(){
+        if (powerUp.powerupName == nametocheck){
+            print("Name Matches!");
+        }
+        else {
+            print("Name Does Not Match!");
+        }
+    }
+}
+ 
---
+++ cube project/Assets/Scripts/PowerUp_SO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName= "Powerup")]
+public class PowerUp_SO : ScriptableObject
+{
+    public string powerupName; 
+}
